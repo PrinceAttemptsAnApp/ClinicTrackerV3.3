3d874d2e020bd600c3c3cc2e63e21a755d2289b3
PrinceAttemptsAnApp <amir2101233@miuegypt.edu.eg>
2026-09-19 18:32:18 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {VitePWA} from 'vite-plugin-pwa';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/ClinicTrackerV3.3/',
     plugins: [
       react(),
       tailwindcss(),
